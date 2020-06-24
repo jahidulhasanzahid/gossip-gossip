71b2b614c5d78061be392cce3fea14d0169361b5
Jahidul Hasan Zahid <55796837+jahidulhasanzahid@users.noreply.github.com>
2020-06-24 22:48:19 +0600

    asset link up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,15 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
 
-    <link href="http://fonts.googleapis.com/css?family=Nunito:300,400,700" rel="stylesheet" type="text/css">
-    <link href="assets/libraries/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="assets/libraries/owl.carousel/assets/owl.carousel.css" rel="stylesheet" type="text/css" >
-    <link href="assets/libraries/colorbox/example1/colorbox.css" rel="stylesheet" type="text/css" >
-    <link href="assets/libraries/bootstrap-select/bootstrap-select.min.css" rel="stylesheet" type="text/css">
-    <link href="assets/libraries/bootstrap-fileinput/fileinput.min.css" rel="stylesheet" type="text/css">
-    <link href="assets/css/superlist.css" rel="stylesheet" type="text/css" >
+    <link href="http://fonts.googleapis.com/css?family=Nunito:300,400,700')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('assets/libraries/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('assets/libraries/owl.carousel/assets/owl.carousel.css')}}" rel="stylesheet" type="text/css" >
+    <link href="{{asset('assets/libraries/colorbox/example1/colorbox.css')}}" rel="stylesheet" type="text/css" >
+    <link href="{{asset('assets/libraries/bootstrap-select/bootstrap-select.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('assets/libraries/bootstrap-fileinput/fileinput.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('assets/css/superlist.css')}}" rel="stylesheet" type="text/css" >
 
-    <link rel="shortcut icon" type="image/x-icon" href="assets/favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/favicon.png')}}">
 
     <title>Superlist - Admin Dashboard</title>
 </head>
@@ -28,7 +28,7 @@
     @include('navbar.blade.php')
 
 
-    
+
     
 </div><!-- /.page-wrapper -->
 
@@ -37,34 +37,34 @@
 
 
 
-<script src="assets/js/jquery.js" type="text/javascript"></script>
-<script src="assets/js/map.js" type="text/javascript"></script>
+<script src="{{asset('assets/js/jquery'.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/js/map'.js')}}" type="text/javascript"></script>
 
-<script src="assets/libraries/bootstrap-sass/javascripts/bootstrap/collapse.js" type="text/javascript"></script>
-<script src="assets/libraries/bootstrap-sass/javascripts/bootstrap/carousel.js" type="text/javascript"></script>
-<script src="assets/libraries/bootstrap-sass/javascripts/bootstrap/transition.js" type="text/javascript"></script>
-<script src="assets/libraries/bootstrap-sass/javascripts/bootstrap/dropdown.js" type="text/javascript"></script>
-<script src="assets/libraries/bootstrap-sass/javascripts/bootstrap/tooltip.js" type="text/javascript"></script>
-<script src="assets/libraries/bootstrap-sass/javascripts/bootstrap/tab.js" type="text/javascript"></script>
-<script src="assets/libraries/bootstrap-sass/javascripts/bootstrap/alert.js" type="text/javascript"></script>
+<script src="{{asset('assets/libraries/bootstrap-sass/javascripts/bootstrap/collapse'.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/libraries/bootstrap-sass/javascripts/bootstrap/carousel'.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/libraries/bootstrap-sass/javascripts/bootstrap/transition'.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/libraries/bootstrap-sass/javascripts/bootstrap/dropdown'.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/libraries/bootstrap-sass/javascripts/bootstrap/tooltip'.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/libraries/bootstrap-sass/javascripts/bootstrap/tab'.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/libraries/bootstrap-sass/javascripts/bootstrap/alert'.js')}}" type="text/javascript"></script>
 
-<script src="assets/libraries/colorbox/jquery.colorbox-min.js" type="text/javascript"></script>
+<script src="{{asset('assets/libraries/colorbox/jquery.colorbox-min'.js')}}" type="text/javascript"></script>
 
-<script src="assets/libraries/flot/jquery.flot.min.js" type="text/javascript"></script>
-<script src="assets/libraries/flot/jquery.flot.spline.js" type="text/javascript"></script>
+<script src="{{asset('assets/libraries/flot/jquery.flot.min'.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/libraries/flot/jquery.flot.spline'.js')}}" type="text/javascript"></script>
 
-<script src="assets/libraries/bootstrap-select/bootstrap-select.min.js" type="text/javascript"></script>
+<script src="{{asset('assets/libraries/bootstrap-select/bootstrap-select.min'.js')}}" type="text/javascript"></script>
 
 <script src="http://maps.googleapis.com/maps/api/js?libraries=weather,geometry,visualization,places,drawing" type="text/javascript"></script>
 
-<script type="text/javascript" src="assets/libraries/jquery-google-map/infobox.js"></script>
-<script type="text/javascript" src="assets/libraries/jquery-google-map/markerclusterer.js"></script>
-<script type="text/javascript" src="assets/libraries/jquery-google-map/jquery-google-map.js"></script>
+<script type="text/javascript" src="{{asset('assets/libraries/jquery-google-map/infobox'.js')}}"></script>
+<script type="text/javascript" src="{{asset('assets/libraries/jquery-google-map/markerclusterer'.js')}}"></script>
+<script type="text/javascript" src="{{asset('assets/libraries/jquery-google-map/jquery-google-map'.js')}}"></script>
 
-<script type="text/javascript" src="assets/libraries/owl.carousel/owl.carousel.js"></script>
-<script type="text/javascript" src="assets/libraries/bootstrap-fileinput/fileinput.min.js"></script>
+<script type="text/javascript" src="{{asset('assets/libraries/owl.carousel/owl.carousel'.js')}}"></script>
+<script type="text/javascript" src="{{asset('assets/libraries/bootstrap-fileinput/fileinput.min'.js')}}"></script>
 
-<script src="assets/js/superlist.js" type="text/javascript"></script>
+<script src="{{asset('assets/js/superlist'.js')}}" type="text/javascript"></script>
 
 </body>
 </html>
